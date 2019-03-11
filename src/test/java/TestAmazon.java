802a89ffed527f85a1818103567b37d5708eb2b3
pshyrkavets <42141532+pshyrkavets@users.noreply.github.com>
2019-03-11 13:34:54 +0300

    Add files via upload

---
+++ src/test/java/TestAmazon.java
@@ -0,0 +1,51 @@
+import com.epam.ta.pages.AmazonPage;
+import driver.DriverSingleton;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import java.time.Duration;
+
+public class TestAmazon {
+    WebDriver driver = DriverSingleton.getDriver();
+    AmazonPage amazonPage = new AmazonPage(driver);
+
+    @Test(description = "I opened Amazon site")
+    public void iOpenedAmazonSite() throws InterruptedException {
+        /*System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\Pavel_Shyrkavets\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();*/
+
+        amazonPage.openPage();
+        //Thread.sleep(5000);
+        //new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("a-page")));
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(5))
+                .ignoring(NoSuchElementException.class);
+        WebElement aPage = wait.until(driver1 -> driver1.findElement(By.id("a-page")));
+        String url = driver.getCurrentUrl();
+
+        Assert.assertEquals(url, "https://www.amazon.com/");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        // Quit the google Chrome driver.
+        if(driver != null)
+        {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
---
+++ src/test/java/TestFillUpComputeEngineForm.java
@@ -0,0 +1,113 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import com.epam.ta.pages.ComputeEnginePage;
+
+public class TestFillUpComputeEngineForm {
+    static WebDriver driver;
+
+
+    @Test(description = "Fill Up Number Of Instances")
+    public void testFillUpComputeEngineForm() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\Pavel_Shyrkavets\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://cloud.google.com/products/calculator/");
+        //Thread.sleep(5000);
+
+        driver.switchTo().frame("idIframe");
+        WebDriverWait waitst = new WebDriverWait(driver, 20);
+        waitst.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id=\"input_46\"]")));
+
+        //Number of instances: 4
+        WebElement numberOfInstances = driver.findElement(By.xpath("//input[@id=\"input_46\"]"));
+        numberOfInstances.sendKeys("4");
+        Thread.sleep(5000);
+
+        //Instance type: n1-standard-8    (vCPUs: 8, RAM: 30 GB)
+        WebElement instanceType = driver.findElement(By.xpath("//*[@id=\"select_value_label_42\"]"));
+        instanceType.click();
+        WebElement n1Standard8 = driver.findElement(By.xpath("//*[@id=\"select_option_70\"]/div"));
+        n1Standard8.click();
+        Thread.sleep(5000);
+
+        //Выбрать Add GPUs
+        WebElement addGPUs = driver.findElement(By
+                .xpath("//*[@id=\"mainForm\"]/div[1]/div/md-card/md-card-content/div/div[1]/form/div[6]/div[1]/md-input-container/md-checkbox/div[1]"));
+        addGPUs.click();
+        //Number of GPUs: 1
+        WebElement numberOfGPUs = driver.findElement(By
+                .xpath("//*[@id=\"select_value_label_327\"]"));
+        numberOfGPUs.click();
+        mouseClickByLocator("#select_option_428");
+        Thread.sleep(10000);
+        //GPU type: NVIDIA Tesla V100
+        WebElement typeOfGPU = driver.findElement(By
+                .xpath("//*[@id=\"select_value_label_328\"]"));
+        typeOfGPU.click();
+        WebElement typeNVIDIATeslaV100 = driver.findElement(By
+                .xpath("//*[@id=\"select_option_371\"]"));
+        typeNVIDIATeslaV100.click();
+        Thread.sleep(5000);
+
+        //Local SSD: 2x375 Gb
+        WebElement localSSD = driver.findElement(By
+                .xpath("//*[@id=\"select_value_label_43\"]"));
+        localSSD.click();
+        WebElement localSSD2x375 = driver.findElement(By
+                .xpath("//*[@id=\"select_option_182\"]/div"));
+        localSSD2x375.click();
+
+        //Datacenter location: Frankfurt (europe-west3)
+        WebElement location = driver.findElement(By
+                .xpath("//*[@id=\"select_value_label_105\"]/span[1]/div"));
+        location.click();
+        WebElement europeWest3 = driver.findElement(By
+                .xpath("//*[@id=\"select_option_230\"]/div"));
+        europeWest3.click();
+
+        //Commited usage: 1 Year
+        WebElement commitedUsage = driver.findElement(By
+                .xpath("//*[@id=\"select_value_label_45\"]"));
+        commitedUsage.click();
+        WebElement oneYear = driver.findElement(By
+                .xpath("//*[@id=\"select_option_100\"]/div"));
+        oneYear.click();
+
+        //Нажать Add to Estimate
+        WebElement addToEstimate = driver.findElement(By
+                .xpath("//*[@id=\"mainForm\"]/div[1]/div/md-card/md-card-content/div/div[1]/form/div[11]/button"));
+        addToEstimate.click();
+
+        WebElement emailEstimate = driver.findElement(By
+                .xpath("//*[@id=\"email_quote\"]"));
+
+        Assert.assertTrue(emailEstimate.isDisplayed());
+    }
+
+    public static void mouseClickByLocator( String cssLocator ) {
+        String locator = cssLocator;
+        WebElement el = driver.findElement( By.cssSelector( locator ) );
+        Actions builder = new Actions(driver);
+        builder.moveToElement( el ).click( el );
+        builder.perform();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        // Quit the google Chrome driver.
+        if(driver != null)
+        {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
